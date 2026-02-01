0f7387ce26c220346fbad64bca469e6f197186d5
yuj2n <dbwls4595@naver.com>
2026-02-01 23:54:30 +0900

    chore-문제 수정

--- yujin-level0/Week39/문자열 내 p와 y의 개수.js
+++ yujin-level0/Week39/문자열 내 p와 y의 개수.js
@@ -1,3 +1,13 @@
+function solution(s) {
+  let count = 0;
+  for (const char of s.toUpperCase()) {
+    if (char === 'P') count++;
+    else if (char === 'Y') count--;
+  }
+  return count === 0;
+}
+
+// 다른 풀이
 function solution(s){
     return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
 }
