871d4b797970b6c2c105650d13fcbb4b5beacf13
yuj2n <dbwls4595@naver.com>
2026-03-08 06:13:36 +0900

    solve: 콜라츠 추측

---
+++ yujin-level0/Week44/콜라츠 추측.js
@@ -0,0 +1,28 @@
+function solution(num) {
+  let count = 0;
+  if (num === 1) {
+    return count;
+  } else {
+    while (num > 1) {
+      num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+      count++;
+      if (count === 500) {
+        return -1;
+      }
+    }
+  }
+  return count;
+}
+
+// 다른 풀이
+// function solution(num) {
+//     let count = 0;
+//     while (count < 500) {
+//         if (num === 1) {
+//             return count;
+//         }
+//         count ++;
+//         num = num % 2 === 0 ? num /2 : num *3 +1;
+//     }
+//     return -1;
+// }
