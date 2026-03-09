1fa214145ea54670e97bdb23bb68d562f0c80e22
yuj2n <dbwls4595@naver.com>
2026-03-10 00:27:30 +0900

    optimize: 불필요한 1인지 체크 로직 제거 및 방어적 프로그래밍 적용

--- yujin-level0/Week44/콜라츠 추측.js
+++ yujin-level0/Week44/콜라츠 추측.js
@@ -1,19 +1,16 @@
 function solution(num) {
   let count = 0;
-  if (num === 1) {
-    return count;
-  } else {
-    while (num > 1) {
-      num = num % 2 === 0 ? num / 2 : num * 3 + 1;
-      count++;
-      if (count === 500) {
-        return -1;
-      }
+
+  while (num > 1) {
+    num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+    count++;
+    if (count >= 500) {
+      return -1;
     }
   }
+
   return count;
 }
-
 // 다른 풀이
 // function solution(num) {
 //     let count = 0;
