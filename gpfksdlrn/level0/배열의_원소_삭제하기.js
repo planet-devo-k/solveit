16e051b9f22ce1f76432da28497bdd0b62e2943f
hizdev <gpfksdlrn@naver.com>
2026-03-21 19:00:05 +0900

    optimize: includes 반복 탐색을 Set 기반 조회로 개선

--- gpfksdlrn/level0/배열의_원소_삭제하기.js
+++ gpfksdlrn/level0/배열의_원소_삭제하기.js
@@ -1,5 +1,8 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181844
 
+// 반복 탐색(includes)을 Set 기반 O(1) 조회로 개선
+// Set: 값의 존재 여부를 빠르게 확인하기 위한 해시 기반 자료구조
 function solution(arr, delete_list) {
-  return arr.filter((num) => !delete_list.includes(num));
+  const setDelete_list = new Set(delete_list);
+  return arr.filter((num) => !setDelete_list.has(num));
 }
