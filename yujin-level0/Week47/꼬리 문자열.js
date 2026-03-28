4df830b86f0072f17fb5f7cea832207228e6efec
yuj2n <dbwls4595@naver.com>
2026-03-29 00:16:15 +0900

    chore: 꼬리문자열 다른풀이 추가

--- yujin-level0/Week47/꼬리 문자열.js
+++ yujin-level0/Week47/꼬리 문자열.js
@@ -1,11 +1,13 @@
 function solution(str_list, ex) {
   let answer = "";
   str_list.forEach((list) => {
-    if (list.includes(ex)) {
-      answer += "";
-    } else {
-      answer += list;
-    }
+    answer += list.includes(ex) ? "" : list;
   });
   return answer;
+}
+
+// 다른 풀이
+function solution(str_list, ex) {
+  // filter는 true를 거르는게 아닌 true를 포함시킴
+  return str_list.filter((str) => !str.includes(ex)).join("");
 }
