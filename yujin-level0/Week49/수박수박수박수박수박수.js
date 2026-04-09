2eae49a4a87228c8f5104d56890a5b0452b92af5
yuj2n <dbwls4595@naver.com>
2026-04-09 19:46:28 +0900

    optimize: 효율성 최적화

--- yujin-level0/Week49/수박수박수박수박수박수.js
+++ yujin-level0/Week49/수박수박수박수박수박수.js
@@ -8,5 +8,5 @@ function solution(n) {
 
 // 다른 풀이: "수박"을 필요한 만큼 반복해서 길게 만든 뒤, 원하는 길이(n)만큼만 잘라내는 방식
 // function solution(n) {
-//     return "수박".repeat(n).slice(0, n);
+//     return '수박'.repeat(n/2) + (n%2 === 1 ? '수' : '');
 // }
