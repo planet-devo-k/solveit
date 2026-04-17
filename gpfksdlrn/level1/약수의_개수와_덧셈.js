50862e4fde3709108d6a137b1e21d989f705108a
hizdev <gpfksdlrn@naver.com>
2026-04-17 14:38:39 +0900

    solve: 약수의 개수와 덧셈

---
+++ gpfksdlrn/level1/약수의_개수와_덧셈.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/77884
+
+function solution(left, right) {
+  let sum = 0;
+
+  for (let i = left; i <= right; i++) {
+    Number.isInteger(Math.sqrt(i)) ? (sum -= i) : (sum += i);
+  }
+  
+  return sum;
+}
