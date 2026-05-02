d54a98ecdcff3a0a5f36857b812ffbf7d28dfb05
sonsujin <sonsj33@gmail.com>
2026-05-02 21:24:50 +0900

    solve: 배열의 길이에 따라 다른 연산 문제 해결

---
+++ sujin/level0/97. 배열의 길이에 따라 다른 연산하기.js
@@ -0,0 +1,12 @@
+function solution(arr, n) {
+  if (arr.length % 2 !== 0) {
+    for (let i = 0; i < arr.length; i++) {
+      if (i % 2 === 0) arr[i] += n;
+    }
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (i % 2 !== 0) arr[i] += n;
+    }
+  }
+  return arr;
+}
