d5dfa4d0e5573eb814f32557aef4d3513a10cd9f
조인성 <leeming01@naver.com>
2026-05-23 23:34:02 +0900

    solve: 직사각형 별찍기 완료

---
+++ Insung-Jo/level_1/직사각형 별찍기.js
@@ -0,0 +1,17 @@
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  const STAR = "*";
+  let answer = "";
+
+  for (let i = 1; i <= b; i++) {
+    for (let j = 1; j <= a; j++) {
+      answer += STAR;
+      if (j === a) answer += "\n";
+    }
+  }
+
+  console.log(answer);
+});
