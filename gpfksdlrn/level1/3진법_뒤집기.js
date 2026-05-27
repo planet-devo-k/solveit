78f530f9f34bef61226efc8da2db094684c1924f
hizdev <gpfksdlrn@naver.com>
2026-05-27 18:14:46 +0900

    solve: 3진법 뒤집기

---
+++ gpfksdlrn/level1/3진법_뒤집기.js
@@ -0,0 +1,6 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/68935
+
+function solution(n) {
+  const reversed = n.toString(3).split('').reverse().join('');
+  return parseInt(reversed, 3);
+}
