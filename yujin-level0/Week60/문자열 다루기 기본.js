fbacbca4637c646b0864275b9934a7f82fa30162
yuj2n <dbwls4595@naver.com>
2026-06-28 19:29:32 +0900

    chore: isNaN 검증 문제로 정규표현식 사용

--- yujin-level0/Week60/문자열 다루기 기본.js
+++ yujin-level0/Week60/문자열 다루기 기본.js
@@ -1,15 +1,7 @@
 function solution(s) {
   if (s.length !== 4 && s.length !== 6) {
-    return false;
+    return /^\d+$/.test(s);
   }
-
-  for (let i of s) {
-    if (isNaN(i)) {
-      return false;
-    }
-  }
-
-  return true;
 }
 
 // 다른 풀이
