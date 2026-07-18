afa95b0de0029e31a46e18d04ab4b6cf00e391e8
gnsals0904 <gnsals0904@naver.com>
2026-07-18 18:34:06 +0900

    solve: 모스부호1

---
+++ gnsals0904/level0/모스부호1.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    static Map<String, Character> morse = new HashMap<>();
+    public String solution(String letter) {
+        makeMorseMap();
+        StringBuilder answer = new StringBuilder();
+
+        for (String code : letter.split(" ")) {
+            answer.append(morse.get(code));
+        }
+
+        return answer.toString();
+    }
+
+    void makeMorseMap(){
+        morse.put(".-", 'a');
+        morse.put("-...", 'b');
+        morse.put("-.-.", 'c');
+        morse.put("-..", 'd');
+        morse.put(".", 'e');
+        morse.put("..-.", 'f');
+        morse.put("--.", 'g');
+        morse.put("....", 'h');
+        morse.put("..", 'i');
+        morse.put(".---", 'j');
+        morse.put("-.-", 'k');
+        morse.put(".-..", 'l');
+        morse.put("--", 'm');
+        morse.put("-.", 'n');
+        morse.put("---", 'o');
+        morse.put(".--.", 'p');
+        morse.put("--.-", 'q');
+        morse.put(".-.", 'r');
+        morse.put("...", 's');
+        morse.put("-", 't');
+        morse.put("..-", 'u');
+        morse.put("...-", 'v');
+        morse.put(".--", 'w');
+        morse.put("-..-", 'x');
+        morse.put("-.--", 'y');
+        morse.put("--..", 'z');
+    }
+}
