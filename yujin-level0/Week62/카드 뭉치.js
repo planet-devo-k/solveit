9c64133cc29ec260549fa391786ca3dac39e0e2c
yuj2n <dbwls4595@naver.com>
2026-07-18 21:37:20 +0900

    chore: 카드 뭉치 문제 시간복잡도 효율화

--- yujin-level0/Week62/카드 뭉치.js
+++ yujin-level0/Week62/카드 뭉치.js
@@ -1,14 +1,17 @@
 function solution(cards1, cards2, goal) {
+  let i = 0;
+  let j = 0;
+
   for (let word of goal) {
-    // cards1의 맨 앞 단어와 일치하면 cards1에서 제거
-    if (cards1[0] === word) {
-      cards1.shift();
+    // cards1의 현재 단어와 일치하는지 확인 (배열 범위를 벗어나지 않았는지도 체크)
+    if (i < cards1.length && cards1[i] === word) {
+      i++;
     }
-    // cards2의 맨 앞 단어와 일치하면 cards2에서 제거
-    else if (cards2[0] === word) {
-      cards2.shift();
+    // cards2의 현재 단어와 일치하는지 확인
+    else if (j < cards2.length && cards2[j] === word) {
+      j++;
     }
-    // 어느 쪽 카드 뭉치의 맨 앞 단어와도 일치하지 않는다면 실패
+    // 둘 다 일치 X
     else {
       return "No";
     }
