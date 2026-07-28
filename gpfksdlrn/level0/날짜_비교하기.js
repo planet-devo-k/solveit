3278933ade2ef84522e503252ee320c4f7e50693
hizdev <70252430+gpfksdlrn@users.noreply.github.com>
2026-07-28 17:10:32 +0900

    `week64` 문제 풀이 (#569)
    * solve: 세 개의 구분자
    
    * solve: 콜라 문제
    
    * solve: 카드 뭉치
    
    * solve: 모스부호(1)
    
    * solve: 2016년
    
    * solve: 과일 장수
    
    * solve: 날짜 비교하기
    
    * solve: 대충 만든 자판
    
    * solve: 덧칠하기

---
+++ gpfksdlrn/level0/날짜_비교하기.js
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181838
+
+function solution(date1, date2) {
+  const firstDate = new Date(date1[0], date1[1] - 1, date1[2]);
+  const secondDate = new Date(date2[0], date2[1] - 1, date2[2]);
+
+  return firstDate >= secondDate ? 0 : 1;
+}
