b807fc6552954e07494bd05d84cf905d9a117bf2
Sienna <sgoldenbird@gmail.com>
2026-08-02 00:10:44 +0900

    solve: 가까운 수

---
+++ sgoldenbird/level0/가까운_수.js
@@ -0,0 +1,12 @@
+function solution(array, n) {
+  const sorted = [...array, n].sort((a, b) => a - b);
+  const index = sorted.indexOf(n);
+
+  if (index === 0) return sorted[1];
+  if (index === sorted.length - 1) return sorted[sorted.length - 2];
+
+  const left = sorted[index - 1];
+  const right = sorted[index + 1];
+
+  return n - left <= right - n ? left : right;
+}
