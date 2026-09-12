0b8ff0454a47319eda111e8f0680acfca587c9c3
hizdev <gpfksdlrn@naver.com>
2026-09-12 18:25:28 +0900

    solve: 바탕화면 정리

---
+++ gpfksdlrn/level1/바탕화면_정리.js
@@ -0,0 +1,20 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/161990
+
+function solution(wallpaper) {
+  let lux = 50;
+  let luy = 50;
+  let rdx = 0;
+  let rdy = 0;
+
+  for (let i = 0; i < wallpaper.length; i++) {
+    for (let j = 0; j < wallpaper[i].length; j++) {
+      if (wallpaper[i][j] === '#') {
+        lux = Math.min(lux, i);
+        luy = Math.min(luy, j);
+        rdx = Math.max(rdx, i);
+        rdy = Math.max(rdy, j);
+      }
+    }
+  }
+  return [lux, luy, rdx + 1, rdy + 1];
+}
